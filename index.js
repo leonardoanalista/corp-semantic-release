39e89ef86726415b006de10a21182c6a58d2e982
Matt Forster <forster.matth@gmail.com>
2016-10-07 01:30:35 -0600

    refactor(modules): Create modules, remove globals
    Working towards what was defined in the roadmap:
    Move functions into their own files.
    Remove any global references (ex. shelljs).
    Update functions to accept parameters that are used within (removing
    external references).

--- index.js
+++ index.js
@@ -1,55 +1,27 @@
 #!/usr/bin/env node
-
 'use strict';
 
-
-const CWD = process.cwd();
-const shelljs = require('shelljs/global');
-const chalk = require('chalk');
+const lib = require('./lib');
+const log = require('./lib').log;
+const shell = require('shelljs');
 const program = require('commander');
-
-
-const path = require('path');
 const fs = require('fs');
-var prependFile = require('prepend-file');
-
-const _errorFn = chalk.bold.red;
-const _successFn = chalk.bold.green;
-const _infoFn = chalk.bold.cyan;
-const LOG = console.log;
-
 var standardChangelog = require('standard-changelog');
 var async = require('async');
 var stream = require('stream');
 
-
-// TODO: change to spread operator and default param value when node supports
-const info = function(msg, obj) {
-  obj = obj || '';
-  LOG(_infoFn(msg), obj);
-};
-const success = function(msg, obj) {
-  obj = obj || '';
-  LOG(_successFn(msg), obj);
-};
-const error = function(msg, obj) {
-  obj = obj || '';
-  LOG(_errorFn(msg), obj);
-};
-
-
 try {
   fs.statSync(process.cwd() + '/package.json');
 } catch (e) {
-  error('Cant find your package.json.');
-  exit(1);
+  log.error('Cant find your package.json.');
+  shell.exit(1);
 }
+
 const pkg = require(process.cwd() + '/package.json');
 
-
 if (!pkg.name || !pkg.version) {
-  error('Minimum required fields in your package.json are name and version.');
-  exit(1);
+  log.error('Minimum required fields in your package.json are name and version.');
+  shell.exit(1);
 }
 
 program
@@ -60,44 +32,33 @@ program
   .option('-v, --verbose', 'Prints debug info')
   .parse(process.argv);
 
-
 if (program.dryrun) {
-  console.info(chalk.bold.bgGreen.white('>> YOU ARE RUNNING IN DRY RUN MODE. NO CHANGES WILL BE MADE <<'));
+  log.announce('>> YOU ARE RUNNING IN DRY RUN MODE. NO CHANGES WILL BE MADE <<');
 }
 
 program.branch = program.branch || 'master';
-
+var newVersion = '';
 
 // ### STEP 0 - Validate branch
-validateBranch();
-
-
-
+lib.validateBranch(program.branch);
 // ### STEP 1 - Work out tags
-const latestTag = getLatestTag();
-
+const latestTag = lib.getLatestTag(program.verbose);
 // ### STEP 2 - Get Commits
-const jsonCommits = getJsonCommits(latestTag);
-
+const jsonCommits = lib.getJsonCommits(latestTag);
 // ### STEP 3 - find out Bump type
-const bumpType = whatBumpFn(jsonCommits);
-
+const bumpType = lib.whatBump(jsonCommits);
 // ### STEP 4 - release or not?
-if (!isReleaseNecessary(bumpType, jsonCommits)) {
-  exit(0);
+if (!lib.isReleaseNecessary(bumpType, latestTag, jsonCommits, program.verbose)) {
+  shell.exit(0);
 }
-
-
 // ### STEP 5 - bump version in package.json (DESTRUCTIVE OPERATION)
-const newVersion = bumpUpVersion(bumpType);
-
+if (!program.dryrun) newVersion = lib.bumpUpVersion(bumpType, latestTag);
 
 async.series([
-
-  // ### STEP 6 - ger changelog contents
+  // ### STEP 6 - get changelog contents
   function(callback) {
     var contentStream = new stream.Writable();
-    contentStream._write = function (chunk, encoding, done) {
+    contentStream._write = function (chunk) {
       callback(null, chunk.toString());
     };
 
@@ -107,217 +68,17 @@ async.series([
   }
 ],
 function(err, results) {
-
   const changes = results[0];
 
   // ### STEP 7 - Write or Append (DESTRUCTIVE OPERATION)
-  writeChangelog(changes); //it has to run after the version has been bumped.
-
+  if (!program.dryrun) {
+    lib.writeChangelog(changes, program.verbose); //it has to run after the version has been bumped.
+  } else {
+    log.info('>>> Changelog contents would have been: \n\n', changes);
+  }
 
   // ### STEP 8 - Run if any pre commit script has been specified (DESTRUCTIVE OPERATION)
-  runPreCommitScript(program.preCommit);
-
+  lib.runPreCommitScript(program.preCommit);
   // ### STEP 9 - Tag and push (DESTRUCTIVE OPERATION)
-  addFilesAndCreateTag(newVersion);
-
+  if (!program.dryrun) lib.addFilesAndCreateTag(newVersion);
 });
-
-
-// #################### Helpers ################### //
-
-
-function getJsonCommits(latestTag) {
-  var rawCommits = exec(`git log -E --format=%H==SPLIT==%B==END== ${latestTag}`, {silent: true}).output;
-
-  var commits = rawCommits.split('==END==\n')
-    .filter(function (raw) {
-      return !!raw.trim();
-    }).map(function (raw) {
-      var data = raw.split('==SPLIT==');
-      return {
-        hash: data[0],
-        message: data[1]
-      };
-    });
-
-  var parsedCommits = commits.map(function (commit) {
-    var gitUtils = require('./gitUtils');
-    return gitUtils.parseRawCommit(commit.hash + '\n' + commit.message);
-  }).filter(function (commit) {
-    return !!commit;
-  });
-
-  return parsedCommits;
-}
-
-function whatBumpFn(parsedCommits) {
-  var type;
-
-  parsedCommits.every(function (commit) {
-    if (commit.breaks.length) {
-      type = 'major';
-      return false;
-    }
-
-    if (commit.type === 'feat') type = 'minor';
-
-    if (!type && commit.type === 'fix') type = 'patch';
-
-    return true;
-  });
-
-  info('>>> Bump type is', type);
-  return type;
-}
-
-function isReleaseNecessary(bumpType, parsedCommits) {
-  if (!bumpType || bumpType === '') {
-    success('\n\nRelease is not necessary at this point. Maybe your commits since your last tag only contains "docs", "style", "refactor", "test" and/or "chore"\n');
-
-    info('---> YOUR LATEST TAG: ', latestTag);
-    if (!program.verbose) info('Run this command again with -v or --verbose to see the commit list from last tag until HEAD.');
-
-    if (program.verbose) {
-      info('PARSED COMMIT LIST SINCE YOUR LATEST TAG:\n');
-
-      info('>>> parsedCommits: ', parsedCommits);
-    }
-
-    return false;
-  } else {
-
-    return true;
-  }
-}
-
-
-
-function writeChangelog(data) {
-  if (program.dryrun) {
-    info('>>> Changelog contents would have been: \n\n', data);
-    return;
-  }
-
-  var writeFileSync = fs.writeFileSync;
-
-  if (program.verbose) info('>>> About to write/append contents to CHANGELOG.md... ');
-
-  var fileName = path.join(process.cwd(), 'CHANGELOG.md');
-
-  prependFile.sync(fileName, data);
-}
-
-
-function runPreCommitScript(script) {
-  if (script) {
-    info(`>>> about to run your "pre-commit" script called "${script}". Command is: npm run ${script}`);
-    exec(`npm run ${script}`).output;
-  }
-}
-
-
-function getLatestTag() {
-
-  var semverValid = require('semver').valid;
-  var regex = /tag:\s*(.+?)[,\)]/gi;
-  var cmd = 'git log --date-order --tags --simplify-by-decoration --pretty=format:"%d"';
-  var data = exec(cmd, {silent: true}).output;
-  var latestTag = null;
-
-  data.split('\n').some(function(decorations) {
-    var match;
-    while (match = regex.exec(decorations)) { // eslint-disable-line no-cond-assign
-      var tag = match[1];
-      if (semverValid(tag)) {
-        latestTag = tag;
-        return true;
-      }
-    }
-  });
-
-  if (latestTag) {
-    if (program.verbose) info('>> Your latest semantic tag is: ', latestTag);
-
-    latestTag = `${latestTag}..HEAD`;
-  } else {
-    info('>> No SemVer tag found. It seems like your first release? Initial release will be set to v1.0.0 as per npmjs specification.');
-    latestTag = 'HEAD';
-  }
-
-  return latestTag;
-}
-
-
-// It executes the release process pipeline
-function addFilesAndCreateTag(newVersion) {
-  if (program.dryrun) return;
-
-  // ###### Add edited files to git #####
-  info('>>> About to add and commit package.json and CHANGELOG...');
-  var code = exec('git add package.json CHANGELOG.md').code;
-  terminateProcess(code);
-
-  // ###### Commit files #####
-  var code = exec(`git commit -m "chore(release): ${newVersion}"`).code;
-  terminateProcess(code);
-
-  // ###### TAG NEW VERSION #####
-  info(`>> Time to create the Semantic Tag: ${newVersion}`);
-  var code = exec(`git tag ${newVersion}`).code;
-  terminateProcess(code);
-
-  // ###### PUSH CHANGES #####
-  info('>>...and push to remote...');
-  exec('git push && git push --tags').output;
-}
-
-
-
-function fromNodeModule(value) {
-  return `${__dirname}/node_modules/${value}`;
-}
-
-function isFirstRelease(latestTag) {
-  return latestTag === 'HEAD';
-}
-
-function bumpUpVersion(bumpType) {
-  // ###### INCREASE VERSION #####
-
-  if (!program.dryrun) {
-    console.log(chalk.bold.cyan('>>> update version on package.json...'));
-    try {
-      var newVersion;
-      if (isFirstRelease(latestTag)) {
-        newVersion = 'v1.0.0';
-        var w = exec(`npm version --no-git-tag-version ${newVersion}`).output.split('\n')[0];
-      } else {
-        newVersion = exec(`npm version --no-git-tag-version ${bumpType}`).output.split('\n')[0];
-      }
-
-      return newVersion;
-    } catch (error) {
-      terminateProcess(1);
-    }
-  }
-
-}
-
-
-function validateBranch() {
-  var branches = exec('git branch').output;
-  var currentBranch = exec('git rev-parse --abbrev-ref HEAD').output.split('\n')[0];
-
-  if (program.branch !== currentBranch) {
-    error('You can not release from branch other than master. Use option --branch to specify branch name.');
-    exit(1);
-  } else {
-    info('>>> Your release branch is: ' + program.branch);
-  }
-}
-
-function terminateProcess(code) {
-  if (code !== 0) {
-    exit(code);
-  }
-}
