34624045f0f93134514dff0385a7b50a3872c019
Brett Uglow <u_glow@hotmail.com>
2017-09-25 19:37:53 +1000

    fix(app): clarify scope-parsing behaviour for issue #50

---
+++ spec/parseRawCommit.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const parseRawCommit = require('../src/lib/parseRawCommit');
+const expect = require('chai').expect;
+
+describe('parseRawCommit', () => {
+  const HASH = '012345HASH';
+  const SUBJECT = 'subject';
+  const BODY = 'body';
+  const FOOTER = 'footer';
+
+  it('should parse a "normal" feature commit', () => {
+    const commit = `${HASH}\nfeat(scope): ${SUBJECT}\n\n${BODY}\n\n${FOOTER}`;
+
+    let msg = parseRawCommit(commit);
+
+    expect(msg).not.equal(null);
+    expect(msg.hash).to.equal(HASH);
+    expect(msg.subject).to.equal(SUBJECT);
+    expect(msg.body).to.equal(`\n${BODY}\n\n${FOOTER}`);
+    expect(msg.type).to.equal('feat');
+    expect(msg.component).to.equal('scope');
+  });
+
+  it('should not parse a feature commit with a scope which contains a "/" to be consistent with other commit message parsing tools', () => {
+    const commit = `${HASH}\nfeat(scope/foo): ${SUBJECT}\n\n${BODY}\n\n${FOOTER}`;
+
+    let msg = parseRawCommit(commit);
+
+    expect(msg).to.equal(null);
+  });
+});
