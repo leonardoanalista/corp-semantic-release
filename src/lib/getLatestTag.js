e6a93be36a6885cf9656a94dfd0edbc576b505a5
Tyler Moses <tmosest@gmail.com>
2018-09-15 15:35:28 -0400

    refactor(app): getLastestTag.js unneeded code
    the default prefix tag is '' which implies that the second if statment isn't needed
    
    ISSUES CLOSED: #65

--- src/lib/getLatestTag.js
+++ src/lib/getLatestTag.js
@@ -16,16 +16,11 @@ module.exports = function getLatestTag(verbose, prefix) {
     while (match = regex.exec(decorations)) { // eslint-disable-line no-cond-assign
       let tag = match[1];
       if (tag.startsWith(prefix)) {
-        const search = tag.replace(prefix, '');
-        console.log(search);
-        if (semverValid(search)) {
+        const semanticVersion = tag.replace(prefix, '');
+        if (semverValid(semanticVersion)) {
           latestTag = tag;
           return true;
         }
-      }
-      if (semverValid(tag)) {
-        latestTag = tag;
-        return true;
       }
     }
   });
